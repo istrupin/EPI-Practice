818c8cb4a9d18ea72d952bdbfbc9932a17f1669d
istrupin <igor.strupinskiy@gmail.com>
2019-12-15 12:18:19 -0800

    implement right propogate

--- src/EPI-Practice.Implementation/Chapters/4. Primitives/Parity/Variants/ModPow2.cs
+++ src/EPI-Practice.Implementation/Chapters/4. Primitives/Parity/Variants/ModPow2.cs
@@ -2,17 +2,17 @@ using System;
 
 namespace EPI_Practice.Implementation.Chapters.Primitives.Parity.Variants
 {
-    public static class RightPropogate
+    public static class ModPow2
     {
         public static uint RightPropogateBits(uint x)
         {
             //this is what we need to propogate
-            var lowestOne = getLowestOne(x);
+            var lowestOne = GetLowestOne(x);
             var mask = lowestOne - 1;
             return x | mask;
 
         }
-        private static uint getLowestOne(uint x) => x&~(x-1);
+        private static uint GetLowestOne(uint x) => x&~(x-1);
     }
 
     
---
+++ src/EPI-Practice.Test/4. Primitives/Parity/Variant/RightPropogateTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using System.Text;
+using EPI_Practice.Implementation.Chapters.Primitives.Parity.Variants;
+using FluentAssertions;
+using Xunit;
+
+namespace EPI_Practice.Test.Primitives.Parity.Variants
+{
+    public class RightPropogateTests
+    {
+        [Theory]
+        [InlineData(12, 15)]
+        [InlineData(2, 3)]
+        [InlineData(4, 7)]
+        [InlineData(80, 95)]
+
+        public void RightPropgateShouldPropogateLastSetBit(uint input, uint expected)
+        {
+            var res = RightPropogate.RightPropogateBits(input).Should().Be(expected);
+        }
+
+        [Fact]
+        public void RightPropogateSetsLastBitOnRandomInputs(){
+            Random rnd = new Random();
+            var rands = Enumerable.Repeat(100000,50).Select(i => rnd.Next(i));
+
+            foreach (var item in rands)
+            {
+                RightPropogate.RightPropogateBits(Convert.ToUInt32(item)).Should().Be(manualRightPropogate(Convert.ToUInt32(item)));
+            }
+            
+        }
+
+        private uint manualRightPropogate(uint x)
+        {
+            var binary = Convert.ToString(x, 2);
+            var reversed = binary.Reverse();
+            StringBuilder sb = new StringBuilder();
+            bool set = false;
+            foreach (var c in reversed)
+            {
+                if(set)
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    if(c == '1'){
+                        sb.Append(c);
+                        set = true;
+                        continue;
+                    }
+                    sb.Append(1);
+                }
+            }
+            var str = new String(sb.ToString().Reverse().ToArray());
+            return Convert.ToUInt32(str, fromBase: 2);
+        }
+    }
+
+
+
+
+}
